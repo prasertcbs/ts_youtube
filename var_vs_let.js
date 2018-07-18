47f6fa7c7438243cc6f4bae2a12f66e05d1a1597
Prasert Kanawattanachai <Prasert@acc.chula.ac.th>
2018-07-18 15:27:29 +0700

    tsc -t es6

--- var_vs_let.js
+++ var_vs_let.js
@@ -1,11 +1,10 @@
-function demo(): void {
+function demo() {
     // var: function scope
     for (var i = 1; i <= 5; i++) {
         console.log(i);
     }
     console.log(`i = ${i}`);
     console.log('------------------');
-
     // let: block scope
     for (let j = 1; j <= 5; j++) {
         console.log(j);
@@ -13,22 +12,16 @@ function demo(): void {
     // j is out of scope
     // console.log(`j =  ${j}`);
     console.log('------------------');
-
 }
-
-// tsc -t es6 
-function ex_from_typescript(): void {
-    let pets = new Set(["Fish", "Cat", "Dog", "Hamster"]);
+function ex_from_typescript() {
+    let pets = new Set(["Cat", "Dog", "Hamster"]);
     pets["species"] = "mammals";
-
     for (let pet in pets) {
         console.log(pet); // "species"
     }
-
     for (let pet of pets) {
         console.log(pet); // "Cat", "Dog", "Hamster"
     }
 }
-
 demo();
 ex_from_typescript();
