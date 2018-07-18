9bd8c62ccb6278fc1f39fe2110a0a4bee51b8842
Prasert Kanawattanachai <Prasert@acc.chula.ac.th>
2018-07-18 13:50:23 +0700

    for each

--- array.ts
+++ array.ts
@@ -1,2 +1,12 @@
 let flowers: string[]=['lily', 'jasmnine', 'carnation']
 console.log(flowers);
+
+// for each *in*
+for (let v in flowers) {
+    console.log(v);
+}
+
+// for each *of*
+for (let v of flowers) {
+    console.log(v);
+}
